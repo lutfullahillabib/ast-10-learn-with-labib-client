99e4b4c10453ddd65de8880c7467e9c9ca7ecfba
lutfullahillabib <lutfullahillabib@gmail.com>
2022-10-26 06:50:22 +0600

    left-side-bar page update

---
+++ src/Components/Courses/LeftSideBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+const LeftSideBar = ({ singleCategory }) => {
+
+    // console.log(singleCategory);
+
+    const { id, category } = singleCategory;
+
+    return (
+        <div className='w-1/2 mx-auto'>
+            <div className='text-start w-3/4 lg:w-5/6 mx-auto font-semibold text-xl py-1 '>
+                <NavLink to={`/categories/${category}`}>
+                    <h2 className=' m-0 py-2 text-black hover:text-white'>
+                        <span className='pr-1'>{id}. </span>
+                        {category}
+                    </h2>
+                </NavLink>
+            </div>
+        </div>
+    );
+};
+
+export default LeftSideBar;
